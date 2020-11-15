fb932ae1bb145dbd49816285f0bff74ebb5656bd
Julio Augusto <julioams@icloud.com>
2020-11-15 13:37:21 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '13ecef9945fbcc985b427d965971a5c5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1c339320-3bb8-4503-a10e-1032a2ab04e2'
+};
